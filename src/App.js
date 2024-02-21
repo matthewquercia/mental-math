af4da572d0587e7d9b9c2c1f621aa59622c811ca
unknown <quercia.matt@gmail.com>
2024-02-20 22:20:53 -0500

    added new features

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import { useState, useEffect, useReducer } from 'react';
+import { useState, useEffect, useReducer, useRef } from 'react';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-  // const [expression, setExpression] = useState(null);
-  // const [input, setInput] = useState("");
-
+  const inputRef = useRef(null);
   const [state, setState] = useReducer((state, newState) => ({...state, ...newState}),
     {
       expression: null,
       input: "",
       numberOfQuestion: 5,
       currentQuestion: 0,
-      inputState: "disabled"
+      inputState: "disabled",
+      questions: []
     }
   )
 
+  useEffect(() => {
+    validateAnswer();
+  }, [state.input])
+
+  useEffect(() => {
+    inputRef.current.focus(); //focuses the input once the game is started
+  }, [state.inputState]) 
+
   const generateExpression = () => {
-    let ops = ['+', '-', "x"]
-    let num1 = Math.floor(Math.random() * (10 - 1) + 1);
-    let num2 = Math.floor(Math.random() * (10 - 1) + 1);
+    let ops = ['+', '-', "x", '/'];
+    let num1 = Math.floor(Math.random() * (12) + 1);
+    let num2 = Math.floor(Math.random() * (12) + 1);
     let operation = ops[Math.floor(Math.random() * ops.length)];
 
+    if(operation === '/'){
+      const factors = Array.from(Array(num1 + 1), (_, i) => i).filter(i => num1 % i === 0);
+      num2 = factors[Math.floor(Math.random() * factors.length)];
+      console.log(factors);
+    }
+
     let arr = num1 > num2 ? [num1, operation, num2] : [num2, operation, num1];
+
     setState({expression: arr, inputState: ""});
   }
 
@@ -33,10 +47,11 @@ function App() {
       if(state.expression[1] === '+') ans = state.expression[0] + state.expression[2];
       if(state.expression[1] === '-') ans = state.expression[0] - state.expression[2];
       if(state.expression[1] === 'x') ans = state.expression[0] * state.expression[2];
+      if(state.expression[1] === '/') ans = state.expression[0] / state.expression[2];
 
       if(ans === parseInt(state.input)){
         generateExpression();
-        setState({input: "", currentQuestion: state.currentQuestion += 1})
+        setState({input: "", currentQuestion: state.currentQuestion += 1, questions: [...state.questions, `${state.expression[0]} ${state.expression[1]} ${state.expression[2]} = ${ans}`]});
       }
 
       if(state.currentQuestion === state.numberOfQuestion){
@@ -49,26 +64,30 @@ function App() {
     setState({input: e.target.value});
   }
 
-  useEffect(() => {
-    validateAnswer();
-  }, [state.input])
-
   const resetGame = () => {
-    setState({expression: null, input: "", numberOfQuestion: 10, currentQuestion: 0, inputState: "disabled"});
+    setState({
+      expression: null, 
+      input: "", 
+      numberOfQuestion: 10, 
+      currentQuestion: 0, 
+      inputState: "disabled",
+      questions: []
+    });
   }
 
   return (
     <div className="container">
       <h1>Mental Math Trainer</h1>
       <div>
-        <meter min="0" max={state.numberOfQuestion} low="0" value={state.currentQuestion}/>
+        {state.expression && <meter min="0" max={state.numberOfQuestion} low="0" value={state.currentQuestion}/>}
       </div>
       <div className="customButton">
-        {!state.expression ? <button className="btn btn-primary" onClick={() => generateExpression()}>Start</button> : <label>{state.expression[0]} {state.expression[1]} {state.expression[2]}</label>}
+        {!state.expression ? <button className="btn btn-primary" onClick={() => generateExpression()}>Start</button> : <label>{state.expression[0]} {state.expression[1]} {state.expression[2]}</label> }
       </div>
-      <input disabled={state.inputState} className="inputField" value={state.input} onChange={(e) => validateInput(e)}/>
+      <input ref={inputRef} type="text" disabled={state.inputState} className="inputField" value={state.input} onChange={(e) => validateInput(e)}/>
       <br />
       <button onClick={() => resetGame()} className="customButton btn btn-danger">Reset</button>
+      {state.questions && state.questions.map(expression => <p key={state.questions.indexOf(expression)}>{expression}</p>)}
     </div>
   );
 }
